67e8f77cf56d1b1bdd8c6f84c497327e7b031f31
Estefanos <65913010+Estefanos8080@users.noreply.github.com>
2021-08-06 08:39:17 -0700

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,6 +1,5 @@
 class Solution {
-     // Can you come up with an algorithm that is less than O(n2) time complexity?
-     // working on it 
+     // o(n**2) 
     public int[] twoSum(int[] nums, int target) {
         int array [] = new int[2];
         for(int x = 0; x < nums.length; x++)
@@ -17,6 +16,26 @@ class Solution {
         return array;
         
     }
+     
+     // Can you come up with an algorithm that is less than O(n2) time complexity?
+     // working on it 
+     // o(n) linear using Hash Map
+    
+    
+        HashMap < Integer, Integer> lis = new HashMap<>();
+        for(int i =0; i<nums.length; i++)
+        {
+            int result = target - nums[i];
+            if(lis.containsKey(result))
+            {
+                return new int [] {lis.get(result), i};
+            }
+            lis.put(nums[i], i);
+        }
+        
+        throw new IllegalArgumentException(" ");
+    
+       }
     
 }
 
